c760a45ff655706f3e8153af0279cf55a3036cc9
Ajay Titus <ajaytitus1386@gmail.com>
2020-12-24 12:46:29 +0530

    Node attempt

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+
+const express = require('express');
+
+const app = express();
+
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+const cors = require('cors');
+app.use(cors()); //Allows one to bypass the CORS policy that stops cross domain access
+app.use(bodyParser.json()); //Helps format the json so it can work between Mongo and JS
+
+
+var PORT = 5500;
+
+app.use(express.static(__dirname));
+
+app.listen(PORT);
