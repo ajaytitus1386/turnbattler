ec9a8b5870f96f7b575446f0a1c89feb34921b95
ajaytitus1386 <72292609+ajaytitus1386@users.noreply.github.com>
2021-01-31 14:39:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
-const {MongoClient} = require('mongodb');
+
 
 const bodyParser = require('body-parser');
 require('dotenv/config');
@@ -19,31 +19,6 @@ const server = http.Server(app);
 
 var PORT = process.env.PORT || 5500;
 
-async function main(){
-    const uri = "mongodb+srv://admin:So92dFEV@cluster0.vldto.mongodb.net/poster?retryWrites=true&w=majority";
-
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    try{
-        await client.connect();
-
-        await listDatabases(client);
-
-    }
-    catch(e){
-        console.error(e);
-    }
-    finally{
-        await client.close();
-    }
-}   
-
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
-
-    console.log("Here they are:");
-    databasesList.databases.forEach(db => console.log(`- ${db.name}`));
-}
-//main().catch(console.error);
 
 app.use(express.static('game'));
 
